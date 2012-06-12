c4147e8f628a6dab1a436ad4c5c8446cbb3e1508
Vadym Tyemirov <temirov@gmail.com>
2012-06-12 12:42:32 +0700

    sysutils changed to handle new property leafs

--- settings/sysutils/index.js
+++ settings/sysutils/index.js
@@ -2,16 +2,16 @@
 //http://onemoredigit.com/post/1527191998/extending-objects-in-node-js
 //All credits go to the author, which is not me
 
+var util = require('util');
+
 Object.defineProperty(Object.prototype, "extend", {
     enumerable: false,
     value: function(from) {
         var props = Object.getOwnPropertyNames(from);
         var dest = this;
         props.forEach(function(name) {
-            if (name in dest) {
                 var destination = Object.getOwnPropertyDescriptor(from, name);
                 Object.defineProperty(dest, name, destination);
-            }
         });
         return this;
      }
